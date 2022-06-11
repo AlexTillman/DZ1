8de4e56ea9c394070f81fdb0562b613ab81948f4
AlexTillman <96126006+AlexTillman@users.noreply.github.com>
2022-06-12 00:28:33 +0300

    Finab DZ

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+
+R = 10;
+H = 20;
+
+S = Math.PI * R * R;
+V = S * H;
+
+line = "-------------------";
+
+console.log("**************");
+console.log("Обьем цилиндра с площадью основы S =" ,S, "радиусом R =",R, "и высотой H =", H);
+console.log (line);
+console.log ("V = ", V);
+console.log (line);
+console.log ("end");
+
